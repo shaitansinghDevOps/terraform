2e1f9f58f31b7a52d54edd870994fd956b1814d4
satyam singh <satyamsingh@satyams-MacBook-Air.local>
2026-06-05 09:10:35 +0530

    first commit

--- 01-nullResource/Terraform-Sample-Test/ec2.tf
+++ 01-nullResource/Terraform-Sample-Test/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "that" {
   ami           = "ami-0738a9da0631f3598"
-  instance_type = "t3.medium"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "Created by Terraform"
