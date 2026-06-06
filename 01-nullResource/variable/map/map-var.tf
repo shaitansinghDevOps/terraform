7a6d72e133354d3ddbd601e0092f4199f372550a
satyam singh <satyamsingh@satyams-MacBook-Air.local>
2026-06-06 11:40:31 +0530

    first commit

--- 01-nullResource/variable/map/map-var.tf
+++ 01-nullResource/variable/map/map-var.tf
@@ -19,3 +19,19 @@ output "apple_description" {
 output "banana_description" {
   value = "my favorite fruit is ${var.map["banana"]}"
 }
+
+variable "map_of_maps" {
+  type = map(map(string))
+  default = {
+    fruits = {
+      apple  = "A sweet red fruit"
+      banana = "A long yellow fruit"
+      cherry = "A small red fruit"
+    }
+    vegetables = {
+      carrot = "A long orange vegetable"
+      broccoli = "A green vegetable"
+    }
+  }
+}
+
