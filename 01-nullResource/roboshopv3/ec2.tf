ff8fe86b5a878ba4e134c7ee169faecf001514f7
satyam singh <satyamsingh@satyams-MacBook-Air.local>
2026-06-11 20:03:27 +0530

    first commit

--- 01-nullResource/roboshopv3/ec2.tf
+++ 01-nullResource/roboshopv3/ec2.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "main" {
- for_each = var.component
+  for_each = var.component
+
   ami           = each.value["ami_id"]
   instance_type = lookup(each.value, "instance_type", "t3.micro")
 
   tags = {
-    Name = each
+    Name = each.key
   }
 }
